79e9264cdf74a16083e0ef28c7678487dd2cbdf0
Akshayunde <akshayunde29@gmail.com>
2020-02-17 23:40:11 +0530

    Add Fibonacci seres program

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+/* 
+  Title  : Print Fibonacci Seres
+  Author : Akshay Unde
+  Date   : 17/02/2020
+*/  
+
+  public class Fibonacci {
+        public static void main(String[] args) {
+            int n = 10, t1 = 0, t2 = 1;
+            System.out.print("First " + n + " terms: ");
+            for (int i = 1; i <= n; ++i)
+            {
+                System.out.print(t1 + " + ");
+                int sum = t1 + t2;
+                t1 = t2;
+                t2 = sum;
+            }
+        }
+    }
